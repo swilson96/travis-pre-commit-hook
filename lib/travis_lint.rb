8b363f4babd92c15783a8bef2fca8c85e5f30df3
Simon Wilson <simon.wilson@digital.cabinet-office.gov.uk>
2017-11-02 17:25:42 +0000

    Exit with error code when needed and fix implementation to fail on warnings

--- lib/travis_lint.rb
+++ lib/travis_lint.rb
@@ -3,6 +3,13 @@ require 'travis/yaml'
 class TravisLint
   def self.validate filename
     puts "Linting: " + filename
-    Travis::Yaml.parse! File.open(filename).read
+    result = Travis::Yaml.parse! File.open(filename).read, filename
+    puts result
+    if (result.nested_warnings.any?)
+      puts "Linting failed"
+      false
+    else
+      true
+    end
   end
 end
--- test/test_lint.rb
+++ test/test_lint.rb
@@ -3,17 +3,22 @@ require 'travis_lint'
 
 class TravisLintTest < Minitest::Test
   def test_minimal_is_valid
-    assert_equal "hello world",
-                 TravisLint.validate('./test/examples/minimal.yml')
+    assert_equal true, TravisLint.validate('./test/examples/minimal.yml')
   end
 
-  def test_empty_is_valid
-    assert_equal "hello world",
-                 TravisLint.validate('./test/examples/empty.yml')
+  def test_example_is_valid
+    assert_equal true, TravisLint.validate('./test/examples/example.yml')
   end
 
-  def test_bad_languages_is_invalid
-    assert_equal "hello world",
-                 TravisLint.validate('./test/examples/bad_language.yml')
+  def test_empty_is_invalid
+    assert_equal false, TravisLint.validate('./test/examples/empty.yml')
+  end
+
+  def test_bad_language_is_invalid
+    assert_equal false, TravisLint.validate('./test/examples/bad_language.yml')
+  end
+
+  def test_bad_yml_is_invalid
+    assert_equal false, TravisLint.validate('./test/examples/not.yml')
   end
 end
